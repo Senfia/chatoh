4b186b440f699f0d994c5074eecbe86f97b84e86
Senfia <senyo332@gmail.com>
2024-02-25 18:36:39 +0000

    fixed content alignment

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -7,37 +7,39 @@ const Login = () => {
         <h1 className="text-3-xl font-semibold text-center">
           Login <span className="text-primary">CHAT OH!</span>
         </h1>
-      </div>
 
-      <form>
-        <div>
-          <label className="label p-2">
-            <span className="text-base label-text">Username</span>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter username"
-            className=" w-full input input-bordered h-10"
-          />
-          <label className="label">
-            <span className="text-base label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            className="w-full input input-bordered h-10"
-          ></input>
-          <a
-            href="#"
-            className="text-sm hover:underline hover:text-accent mt-2 inline-block text-left"
-          >
-            Don't have an account?
-          </a>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className=" w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              className="w-full input input-bordered h-10"
+            ></input>
+            <a
+              href="#"
+              className="text-sm hover:underline hover:text-accent mt-2 inline-block text-left"
+            >
+              Don't have an account?
+            </a>
+          </div>
 
           <div className="btn btn-block btn-sm mt-4 bg-primary text-base-100 hover:bg-accent">
             Login
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
