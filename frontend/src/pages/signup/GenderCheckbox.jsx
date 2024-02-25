8ffc5a22d24d6162bc56a2ba55d0d1add4e9c90c
Senfia <senyo332@gmail.com>
2024-02-25 19:28:26 +0000

    fixed styling issue

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const GenderCheckbox = () => {
   return (
     <div className="flex">
-      <div className="form-control p-2">
-        <label className="{`label gap-5 cursor-pointer`}">
-          <span className="label-text p-2">Male</span>
-          <input type="checkbox" className="checkbox border-primary" />
+      <div className="form-control">
+        <label className="cursor-pointer gap-2 label">
+          <span className="label-text">Male</span>
+          <input type="checkbox" className="checkbox checkbox-accent" />
         </label>
       </div>
-      <div className="form-control p-2">
-        <label className="{`label gap-2 cursor-pointer`}">
-          <span className="label-text p-2">Female</span>
-          <input type="checkbox" className="checkbox border-primary" />
+      <div className="form-control">
+        <label className="cursor-pointer gap-2 label">
+          <span className="label-text">Female</span>
+          <input type="checkbox" className="checkbox checkbox-accent" />
         </label>
       </div>
     </div>
