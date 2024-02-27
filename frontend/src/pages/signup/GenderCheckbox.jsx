75b92572823137c7dc249dc3e92f7d0871133a36
Senfia <senyo332@gmail.com>
2024-02-27 10:24:12 +0000

    added selection logic

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className="cursor-pointer gap-2 label">
+        <label
+          className={`cursor-pointer gap-2 label" ${
+            selectedGender === "male" ? "selected" : ""
+          } `}
+        >
           <span className="label-text">Male</span>
-          <input type="checkbox" className="checkbox checkbox-accent" />
+          <input
+            type="checkbox"
+            className="checkbox checkbox-accent"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
       <div className="form-control">
-        <label className="cursor-pointer gap-2 label">
+        <label
+          className={`cursor-pointer gap-2 label ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Female</span>
-          <input type="checkbox" className="checkbox checkbox-accent" />
+          <input
+            type="checkbox"
+            className="checkbox checkbox-accent"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
