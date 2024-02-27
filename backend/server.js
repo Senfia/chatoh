66376ed9217e542e5a24c05b0c67885c11a34aa9
joseph darko <osomfonashticah@gmail.com>
2024-02-27 01:41:57 +0000

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
@@ -9,8 +14,12 @@ const port = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   connectToMongoDB();
