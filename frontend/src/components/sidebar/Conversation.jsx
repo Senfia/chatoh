4b819f6d167009fead9ed057cf65db0ce2cc5ad9
Senfia <senyo332@gmail.com>
2024-02-28 21:27:28 +0000

    fixed loading error

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,22 @@
-const Conversation = (conversation, lastIdx) => {
+import { useSocketContext } from "../../context/SocketContext";
+import useConversation from "../../zustand/useConversation";
+
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext() || {}; // Add null check here
+  const isOnline = onlineUsers?.includes(conversation._id); // Add null check here
+
   return (
     <>
-      <div className="flex gap-2 item-ceter hover:bg-accent rounded p-2 py-1 cursor-pointer">
-        <div className="avatar online">
+      <div
+        className={`flex gap-2 item-ceter hover:bg-accent rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-accent" : ""}
+			`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
@@ -11,11 +25,12 @@ const Conversation = (conversation, lastIdx) => {
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
             <p className="font-bold text-gray-200">{conversation.fullName}</p>
-            {/* <span className=""></span> */}
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
-      {!lastIdx && <div className="divider my-0 py-1 h-0.25" />}
+
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
