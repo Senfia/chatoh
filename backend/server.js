a241b73af00db3f3488fd5da9720fccb4f4fc3f8
Senfia <senyo332@gmail.com>
2024-02-29 15:28:36 +0000

    added static path for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -19,6 +22,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// Serve static assets if in production
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// Serve the index.html file if in production
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
 
 server.listen(port, () => {
   connectToMongoDB();
