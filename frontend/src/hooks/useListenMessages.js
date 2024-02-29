eeb359d1fde0f7a70557182c9c054396fc95703c
Senfia <senyo332@gmail.com>
2024-02-29 12:38:41 +0000

    audio sound effect added

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -11,14 +11,16 @@ const useListenMessages = () => {
   const { messages, setMessages } = useConversation();
 
   useEffect(() => {
-    socket?.on("newMessage", (newMessage) => {
-      newMessage.shouldShake = true;
-      const sound = new Audio(notificationSound);
-      sound.play();
-      setMessages([...messages, newMessage]);
-    });
+    if (socket) {
+      socket.on("newMessage", (newMessage) => {
+        newMessage.shouldShake = true;
+        const sound = new Audio(notificationSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      });
 
-    return () => socket?.off("newMessage");
+      return () => socket.off("newMessage");
+    }
   }, [socket, setMessages, messages]);
 };
 export default useListenMessages;
