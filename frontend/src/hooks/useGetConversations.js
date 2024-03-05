20228b80d3a4fea52f862d62cf25d55bb66800d7
Senfia <senyo332@gmail.com>
2024-03-05 22:47:17 +0000

    added documentation

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -1,3 +1,11 @@
+/**
+ * Custom hook to fetch conversations from the server.
+ *
+ * @returns {Object} An object containing the loading state and conversations array.
+ * @property {boolean} loading - A boolean indicating whether the conversations are being loaded or not.
+ * @property {Array} conversations - An array of conversation objects.
+ */
+
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
