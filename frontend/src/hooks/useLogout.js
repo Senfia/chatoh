fd907c94c7eaf4edc07f273ca6ca2d7fabc31600
Senfia <senyo332@gmail.com>
2024-03-05 22:47:03 +0000

    added documentation

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,3 +1,11 @@
+/**
+ * Custom hook for handling user logout functionality.
+ *
+ * @returns {Object} An object containing the loading state and the logout function.
+ * @property {boolean} loading - A boolean indicating whether the logout process is currently in progress.
+ * @property {Function} logout - A function that initiates the logout process.
+ */
+
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
